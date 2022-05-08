1a6a22bb7043bc62a4a8bbc9194b1cc90eb0e276
prince <janprince002@gmail.com>
2022-05-08 01:47:36 +0000

    update unit test notes: edge cases, additional test cases

--- Interact_OS_Python/Testing/unit_testing/rearrange_test.py
+++ Interact_OS_Python/Testing/unit_testing/rearrange_test.py
@@ -9,5 +9,33 @@ class TestRearrange(unittest.TestCase):
         expected = "Ada Lovelace"
         return self.assertEqual(rearrange_name(testcase), expected)
 
+    def test_empty(self):
+        testcase = ""
+        expected = ""
+        return self.assertEqual(rearrange_name(testcase), expected)
+
+    def test_double_name(self):
+        testcase = "Hopper, Grace M."
+        expected = "Grace M. Hopper"
+        return self.assertEqual(rearrange_name(testcase), expected)
+
+    def test_one_name(self):
+        testcase = "Voltaire"
+        expected = "Voltaire"
+        return self.assertEqual(rearrange_name(testcase), expected)
+
+
 if __name__ == '__main__':
     unittest.main()
+
+
+"""
+        Edge Cases
+        Edge cases are inputs to our code that produce unexpected results, and are found at the extreme ends of 
+        the ranges of input we imagine our programs will typically work with. Edge cases usually need special 
+        handling in scripts in order for the code to continue to behave correctly. 
+        
+         Other kinds of edge cases usually include things like passing zero to a function that expects a number, 
+         or negative numbers, or extremely large numbers. These types of conditions are good to consider when writing 
+         your test, since they can cause your code to crash or behave in unexpected ways.
+"""
